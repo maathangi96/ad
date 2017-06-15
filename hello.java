8028fd27af6b656dbe3ebd9155eebe03f3c5a527
maathangi96 <maathangi96@gmail.com>
2017-06-15 15:31:42 +0530

    hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class hello
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println("Hello");
+        }
+        
+    }
+}
