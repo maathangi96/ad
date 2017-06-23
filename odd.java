4fe5cfbba396e2679e157f95c3a027d71a5bf5a0
maathangi96 <maathangi96@gmail.com>
2017-06-23 21:00:35 +0530

    odd.java

---
+++ odd.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+class odd
+{
+public static void main(String args[])
+{
+//Scanner s=new Scanner(System.in);
+//int i=s.nextInt();
+for(int i=0;i<=10;i++)
+//{
+if(i%2==1)
+{
+System.out.println(i);
+}
+}
+}
