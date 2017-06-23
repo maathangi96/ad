88ced035230f54314d072d7c67d683c78029e56c
maathangi96 <maathangi96@gmail.com>
2017-06-23 21:03:39 +0530

    odd.java

--- odd.java
+++ odd.java
@@ -4,9 +4,9 @@ class odd
 {
 public static void main(String args[])
 {
-//Scanner s=new Scanner(System.in);
-//int i=s.nextInt();
-for(int i=0;i<=10;i++)
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+for(int i=0;i<=n;i++)
 //{
 if(i%2==1)
 {
